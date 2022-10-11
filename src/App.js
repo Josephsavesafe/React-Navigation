28b5218de33b0298bc2e0d62ca38a8af6ad0faee
josephwindows <tanawut247@gmail.com>
2022-10-11 12:08:19 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navigation from "./components/Navigation";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Page/Home";
+import About from "./components/Page/About";
+import Contact from "./components/Page/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/MenuData.js
@@ -0,0 +1,21 @@
+import { FaHome, FaBookReader, FaFileContract } from "react-icons/fa";
+
+const MenuData = [
+  {
+    icon: <FaHome />,
+    title: "หน้าแรก",
+    path: "/",
+  },
+  {
+    icon: <FaBookReader />,
+    title: "เกี่ยวกับ",
+    path: "/about",
+  },
+  {
+    icon: <FaFileContract />,
+    title: "ติดต่อ",
+    path: "/contact",
+  },
+];
+
+export default MenuData;
